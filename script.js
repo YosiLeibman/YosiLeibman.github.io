50467a6f58e5ace856e6dd75e303f65a1e6f558d
YosiLeibman <yosil.770@gmail.com>
2019-06-21 10:47:57 +0300

    fixed bug: if you search for name or partial name with more than one match - now it will show you all of them

--- script.js
+++ script.js
@@ -19,76 +19,66 @@ let tempNewCurrElement
 let tempNewCurrValElement
 let tempNewIMGElement
 
+let drawSec = (countries) => {
+    console.log(countries)
+    for(let country of countries){
+        console.log(country.name)
+        tempNewSectionElement = document.createElement("section")
+        document.body.appendChild(tempNewSectionElement)
+        tempNewSectionElement.className= "each-all-sec"
+        tempNewFlagElement = document.createElement("div")
+        tempNewFlagElement.className="flag"
+        tempNewFlagElement.style.backgroundImage = `url('${country.flag}')` 
+        // tempNewFlagElement.style.backgroundSize = 'cover' 
+        tempNewSectionElement.appendChild(tempNewFlagElement)
+        tempNewDetailsElement = document.createElement("div") 
+        tempNewDetailsElement.className="details"
+        tempNewSectionElement.appendChild(tempNewDetailsElement)
+        // name fildes
+        tempNewNameElement = document.createElement("h2")
+        tempNewNameElement.textContent = "Name:"
+        tempNewNameValElement = document.createElement("h2")
+        tempNewNameValElement.textContent = country.name
+        tempNewNameValElement.className = "txt"
+        tempNewDetailsElement.appendChild(tempNewNameElement)
+        tempNewDetailsElement.appendChild(tempNewNameValElement)
+        // topleveldomain fildes
+        tempNewTLPElement = document.createElement("h2")
+        tempNewTLPElement.textContent = "Top Level Domain:"
+        tempNewTLPValElement = document.createElement("h2")
+        tempNewTLPValElement.textContent = country.topLevelDomain
+        tempNewTLPValElement.className = "txt"
+        tempNewDetailsElement.appendChild(tempNewTLPElement)
+        tempNewDetailsElement.appendChild(tempNewTLPValElement)
+        // capital fildes
+        tempNewCapitalElement = document.createElement("h2")
+        tempNewCapitalElement.textContent = "capital:"
+        tempNewCapitalValElement = document.createElement("h2")
+        tempNewCapitalValElement.textContent = country.capital
+        tempNewCapitalValElement.className = "txt"
+        tempNewDetailsElement.appendChild(tempNewCapitalElement)
+        tempNewDetailsElement.appendChild(tempNewCapitalValElement)
+        // currency fildes
+        tempNewCurrElement = document.createElement("h2")
+        tempNewCurrElement.textContent = "currency:"
+        tempNewCurrValElement = document.createElement("h2")
+        tempNewCurrValElement.textContent = JSON.stringify(country.currencies)
+        tempNewCurrValElement.className = "txt"
+        tempNewDetailsElement.appendChild(tempNewCurrElement)
+        tempNewDetailsElement.appendChild(tempNewCurrValElement)
+    }
+}
+
 searchBtn.addEventListener("click", () => {
     let url = `https://restcountries.eu/rest/v2/name/${searchInput.value}?fields=name;capital;flag;topLevelDomain;currencies`
     console.log(url)
-    $.get(url, (res) => {
-        console.log(res[0])
-        name.innerHTML = res[0].name
-        topLevelDomain.innerHTML = res[0].topLevelDomain
-        capital.innerHTML = res[0].capital
-        currency.innerHTML = JSON.stringify(res[0].currencies)
-        console.log(JSON.stringify(res[0].currencies)) 
-        flagImg.style.backgroundImage = `url('${res[0].flag}')` 
-    })
+    $.get(url,drawSec)
 })
 
 allBtn.addEventListener("click", () => {
-    document.querySelector("#main-sec").style.display="none"
     let url = `https://restcountries.eu/rest/v2/all/?fields=name;capital;flag;topLevelDomain;currencies`
-    console.log(url)
-    $.get(url, (res) => {
-        console.log(res)
-        for(let country of res){
-            console.log(country.name)
-            tempNewSectionElement = document.createElement("section")
-            document.body.appendChild(tempNewSectionElement)
-            tempNewSectionElement.className= "each-all-sec"
-            tempNewFlagElement = document.createElement("div")
-            tempNewFlagElement.className="flag"
-            tempNewFlagElement.style.backgroundImage = `url('${country.flag}')` 
-            // tempNewFlagElement.style.backgroundSize = 'cover' 
-            tempNewSectionElement.appendChild(tempNewFlagElement)
-            tempNewDetailsElement = document.createElement("div") 
-            tempNewDetailsElement.className="details"
-            tempNewSectionElement.appendChild(tempNewDetailsElement)
-
-            // name fildes
-            tempNewNameElement = document.createElement("h2")
-            tempNewNameElement.textContent = "Name:"
-            tempNewNameValElement = document.createElement("h2")
-            tempNewNameValElement.textContent = country.name
-            tempNewNameValElement.className = "txt"
-            tempNewDetailsElement.appendChild(tempNewNameElement)
-            tempNewDetailsElement.appendChild(tempNewNameValElement)
-
-            // topleveldomain fildes
-            tempNewTLPElement = document.createElement("h2")
-            tempNewTLPElement.textContent = "Top Level Domain:"
-            tempNewTLPValElement = document.createElement("h2")
-            tempNewTLPValElement.textContent = country.topLevelDomain
-            tempNewTLPValElement.className = "txt"
-            tempNewDetailsElement.appendChild(tempNewTLPElement)
-            tempNewDetailsElement.appendChild(tempNewTLPValElement)
-
-             // capital fildes
-             tempNewCapitalElement = document.createElement("h2")
-             tempNewCapitalElement.textContent = "capital:"
-             tempNewCapitalValElement = document.createElement("h2")
-             tempNewCapitalValElement.textContent = country.capital
-             tempNewCapitalValElement.className = "txt"
-             tempNewDetailsElement.appendChild(tempNewCapitalElement)
-             tempNewDetailsElement.appendChild(tempNewCapitalValElement)
-
-             // currency fildes
-             tempNewCurrElement = document.createElement("h2")
-             tempNewCurrElement.textContent = "currency:"
-             tempNewCurrValElement = document.createElement("h2")
-             tempNewCurrValElement.textContent = JSON.stringify(country.currencies)
-             tempNewCurrValElement.className = "txt"
-             tempNewDetailsElement.appendChild(tempNewCurrElement)
-             tempNewDetailsElement.appendChild(tempNewCurrValElement)
-
-        }
-    })
+    $.get(url,drawSec)
 })
+
+
+
